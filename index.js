dd90baf6a2239f4fc11fd1c43866d9746e57ee2b
Vytautas Klimavicius <vyklimavicius@gmail.com>
2018-12-08 15:19:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeAnumber(people,name){
+var number = [];
+let i = 0
+while (i < people){
+number.push(`Welcome, ${name[i]}. You are number ${i+1}`)
+katzDeliLine.push(name[i]);
+i++;
+}
+return number;
+}
